1544640e3ef1c081f0f002c11d97b9e16432f208
Ryan <34758@ma-web.nl>
2024-03-14 10:11:47 +0100

    les 3.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,21 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        
     ],
 
     theme: {
+        colors: {
+            'blue': '#1fb6ff',
+            'purple': '#7e5bef',
+            'pink': '#ff49db',
+            'orange': '#ff7849',
+            'green': '#13ce66',
+            'yellow': '#ffc82c',
+            'gray-dark': '#273444',
+            'gray': '#8492a6',
+            'gray-light': '#d3dce6',
+          },
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],

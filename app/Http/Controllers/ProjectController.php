fa27d29e8957f2b980a673b0aba73926166a527d
Ryan <34758@ma-web.nl>
2024-04-11 09:39:49 +0200

    les 5.2 af

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,15 +1,31 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
+ 
 use Illuminate\Http\Request;
-
+use App\Models\Project;
+ 
+ 
 class ProjectController extends Controller
 {
-    //
     public function add() {
-        $project->titel       = 'Mijn titel';
-        $project->description = 'mijn verhaal';
-        $project->active      = true;   
+        $projects = new Project();
+        // definieer de velden
+        $projects->title       = 'Ryan is de best';
+        $projects->description = 'mijn verhaal is de best';
+        $projects->active      = true;
+        // sla het model op
+        $projects->save();      
+    }
+ 
+    public function index() {
+        $projects = Project::all();
+ 
+        return view(
+            'projects.index',
+            [
+                'projects' => $projects,
+            ]
+        );
     }
 }
